36920ce082877236457a52630556d154785a1365
diana24802 <diana.orsolyova@student.tuke.sk>
2024-02-04 23:00:33 +0100

    dianka was here

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="the-only-one">Dianka was here.</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.the-only-one {
+  width:100%;
+  height:100vh;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#app {
+  font-family: Roboto, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  background-color: #181818;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+</style>

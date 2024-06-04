a32d8e8dae07fd39d43c0e35c277aaa8f82e8d1f
fromDiana <orsolyovadianka@gmail.com>
2024-06-05 01:35:10 +0200

    added tymo link

--- src/views/LinksPage.vue
+++ src/views/LinksPage.vue
@@ -1,9 +1,10 @@
 <template>
-    <div style="display: flex; flex-direction: column; gap: 1rem;">
+    <div style="display: flex; flex-direction: column; gap: 1rem; margin-top: 5rem;">
     <h1>Links</h1>
 
     <a href="rst.sk"> > RST - website I created for a client</a>
     <a href="gamedays.sk"> > GameDays - gaming conference for which I manage website in WordPress</a>
+    <a href="https://www.tymobeauty.com/DIANA44602"> > TYMO 20% discount ( code: DIANA44602 )</a>
     </div>
 </template>
 

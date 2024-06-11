06289bccb506c7407e676943fc7cf0ef56ddd78a
fromDiana <diana.orsolyova@student.tuke.sk>
2024-06-11 19:19:40 +0200

    adjusted text on links page

--- src/views/LinksPage.vue
+++ src/views/LinksPage.vue
@@ -2,7 +2,7 @@
     <div style="display: flex; flex-direction: column; gap: 1rem; margin-top: 5rem;">
     <h1>Links</h1>
 
-    <router-link to="/kpi">KPI</router-link>
+    <router-link to="/kpi">> KPI - website I created with my classmates for our school</router-link>
     <a href="rst.sk"> > RST - website I created for a client</a>
     <a href="gamedays.sk"> > GameDays - gaming conference for which I manage website in WordPress</a>
     <a href="https://www.tymobeauty.com/DIANA44602"> > TYMO 20% discount ( code: DIANA44602 )</a>
